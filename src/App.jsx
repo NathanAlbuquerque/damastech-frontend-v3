fafa51c7c7590437cdb13f14355ea083459587c6
Nathan Albuquerque <nathan.nra@outlook.com>
2025-03-02 18:03:42 -0300

    funcionalidade: sessao cadastro de empresa

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Banner from './Banner.jsx'
 import Depoimentos from './Depoimentos.jsx'
 import Servicos from './Servicos.jsx'
 import Jornada from './Jornada.jsx'
+import Cadastro from './Cadastro.jsx'
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Depoimentos/>
         <Servicos/>
         <Jornada/>
+        <Cadastro/>
       </main>
     </>
   )
---
+++ src/Cadastro.jsx
@@ -0,0 +1,37 @@
+import Titulo from './Titulo.jsx'
+import Botao from './Botao.jsx'
+import Container from './Container.jsx'
+import EntrarIcone from './icones/EntrarIcone.jsx'
+// imagens
+import damastechLogo from './assets/logo.svg'
+
+export default function Cadastro() {
+    return (
+        <>
+            <Container>
+                <Titulo>Crie sua jornada conosco</Titulo>
+
+                <div className="flex items-center justify-around gap-x-10 mt-15">
+                    <div className='w-full flex flex-col items-center'>
+                        <img src={damastechLogo} className="" width={300} alt="damastech"></img>
+                        <p className="text-md text-damastech font-semibold text-center px-20">Somos uma plataforma de empregabilidade voltada para a comunidade feminina</p>
+                    </div>
+                    <div className='w-full flex flex-col gap-y-7 p-10 border border-damastech rounded-2xl'>
+                        <h2 className="text-3xl text-damastech font-semibold text-center">Comece agora</h2>
+                        <input type="text" className="border-2 border-damastech rounded-xl h-10 pl-4 placeholder:font-bold" placeholder='Nome da empresa' maxLength={255} autoComplete='off' required />
+                        <input type="text" className="border-2 border-damastech rounded-xl h-10 pl-4 placeholder:font-bold" placeholder='CNPJ' maxLength={255} autoComplete='off' required />
+                        <input type="text" className="border-2 border-damastech rounded-xl h-10 pl-4 placeholder:font-bold" placeholder='@Email' maxLength={255} autoComplete='off' required />
+                        <input type="text" className="border-2 border-damastech rounded-xl h-10 pl-4 placeholder:font-bold" placeholder='*Senha' maxLength={255} autoComplete='off' required />
+                        <input type="text" className="border-2 border-damastech rounded-xl h-10 pl-4 placeholder:font-bold" placeholder='*Confirmar Senha' maxLength={255} autoComplete='off' required />
+                        <button className="bg-damastech hover:bg-damastech-900 py-2 rounded-2xl text-white text-lg font-semibold cursor-pointer transition">
+                            Cadastrar
+                        </button>
+                    </div>
+                </div>
+                <div className="flex justify-center mt-15">
+                    <Botao>SAIBA MAIS <EntrarIcone cor="white"/></Botao>
+                </div>
+            </Container>
+        </>
+    )
+}
