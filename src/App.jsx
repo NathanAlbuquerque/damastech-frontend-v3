6b20d9739164dccdc00b90672145f5cc5823805f
Nathan Albuquerque <nathan.nra@outlook.com>
2025-03-04 16:40:15 -0300

    funcionalidade: instalacao do React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-// import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cabecalho from './Cabecalho.jsx'
-import Banner from './Banner.jsx'
-import Depoimentos from './Depoimentos.jsx'
-import Servicos from './Servicos.jsx'
-import Jornada from './Jornada.jsx'
-import Cadastro from './Cadastro.jsx'
-import Novidades from './Novidades.jsx'
 import Rodape from './Rodape.jsx'
+import Empresa from './Empresa.jsx'
 
 export default function App() {
   return (
     <>
       <Cabecalho/>
       <main>
-        <Banner/>
-        <Depoimentos/>
-        <Servicos/>
-        <Jornada/>
-        <Cadastro/>
-        <Novidades/>
-        <Rodape/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/empresa" element={<Empresa/>}/>
+          </Routes>
+        </BrowserRouter>
       </main>
+      <Rodape/>
     </>
   )
 }
---
+++ src/Empresa.jsx
@@ -0,0 +1,19 @@
+import Banner from './Banner.jsx'
+import Depoimentos from './Depoimentos.jsx'
+import Servicos from './Servicos.jsx'
+import Jornada from './Jornada.jsx'
+import Cadastro from './Cadastro.jsx'
+import Novidades from './Novidades.jsx'
+
+export default function Empresa () {
+    return (
+        <>
+            <Banner/>
+            <Depoimentos/>
+            <Servicos/>
+            <Jornada/>
+            <Cadastro/>
+            <Novidades/>
+        </>
+    )
+}
